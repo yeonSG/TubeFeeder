af810e5a2ddf261c13d64a6722129daaaaa0957e
yeonSG <dustjd43@gmail.com>
2020-12-08 00:29:53 +0900

    추가 : (#4)
    - 제어장치 코드 구현 (ControlBoard.cs)
     - 메시지 protocol 추가 (MessageProtocol.cs)
     - 메시지 처리기 추가 (MessageReciver.cs,  MessageGenerator.cs)
     - 메시지 write, read 부분 구현 (TubeFeeder.cs)
    
    제거 :
     - 컴포트 설정 UI 제거
    
    변경 :
     - 테스트코드 변경

---
+++ TubeFeederProject/TubeFeeder/ControlBoard.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TubeFeeder
+{
+    /* 컨트롤 보드에 대한 구현
+     * - Comport 연결관련 구현
+     * - Message 프로토콜과 처리 관련된 구현
+     */
+    class ControlBoard
+    {
+        /* 컴포트 설정 */
+        public const String CONTROLBOARD_COMPORT = "COM4";
+        public const int COM_BAUDRATE = 115200;
+        public const int COM_DATABITS = 8;
+        public const Parity COM_PARITY = Parity.None;
+        public const StopBits COM_STOPBITS = StopBits.One;
+
+        private SetTextCallback LogFunction = null;
+
+        private SerialPort m_serialPort = null;
+        private MessageReciver m_messageReciver = new MessageReciver();
+
+        public static bool m_controlBoardConnected = false; // 컨트롤 보드 연결 성공 시 True
+        
+
+        private MessageReciver m_messageProcessor = null;
+
+        public ControlBoard(SerialPort serialPort, SetTextCallback logFunction)
+        {
+            this.m_serialPort = serialPort;
+            this.LogFunction = logFunction;
+
+            this.m_messageProcessor = new MessageReciver();
+
+            Init();
+            
+        }
+
+        private void Init()
+        {
+            m_serialPort.PortName = CONTROLBOARD_COMPORT;
+            m_serialPort.BaudRate = COM_BAUDRATE;
+            m_serialPort.DataBits = COM_DATABITS;
+            m_serialPort.Parity = COM_PARITY;
+            m_serialPort.StopBits = COM_STOPBITS;
+
+            Open();
+        }
+
+        public bool Open()
+        {
+            try
+            {
+                m_serialPort.Open();
+                LogFunction("Port "+ m_serialPort.PortName +" is Opend.");
+            }
+            catch
+            {
+                LogFunction("Port is already using.");
+                return false;
+            }
+
+            LogFunction("Port Opened");
+            return true;
+        }
+
+        public bool isOpen()
+        {
+            return m_serialPort.IsOpen;
+        }
+
+        private void setBoardEnabled(bool enabled)
+        {
+            m_controlBoardConnected = enabled;
+        }
+
+        public bool SendMessage(byte[] msg)
+        {
+            if (isOpen() == false)
+                return false;
+
+            try
+            {
+                //m_serialPort.Write(msg, 0, msg.Length);
+                
+                //if(doDelay==true)
+                //    Thread.Sleep(100);
+
+                int sendSize = msg.Length;
+
+                //serialPort1.Write(data, 0, sendSize);
+                for (int i = 0; i < sendSize; i++ )
+                {
+                    Wait.Start(10);      // MicroSec Sleep
+                    m_serialPort.Write(msg, i, 1);
+                }
+                 
+                string sendString = "[TX] ";
+                for (int i = 0; i < sendSize; i++)
+                {
+                    sendString += msg[i].ToString("X2") + " ";
+                }
+                LogFunction(sendString + "\r\n");
+
+            }
+            catch
+            {
+                LogFunction("Error at SendMessage()");
+                return false;
+            }
+            return true;
+        }
+
+        public void ProcessMessage(byte[] msg)
+        {
+            if (m_messageReciver.messageProcessing(msg) == false)
+                LogFunction("Error at messageProcessing");
+            
+        }
+
+
+    }
+}
---
+++ TubeFeederProject/TubeFeeder/MessageGenerator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TubeFeeder
+{
+    class MessageGenerator
+    {
+
+        // 제어장치에게 Ping 보냄
+        public static byte[] Meesage_Ping()
+        {
+            byte[] Message = new byte[7];
+
+            Message[MessageProtocol.PROTOCOL_HEADER] = MessageProtocol.HEADER;
+            Message[MessageProtocol.PROTOCOL_CMD] = MessageProtocol.CMD_PING;
+            Message[MessageProtocol.PROTOCOL_REG] = 0x00;
+            Message[MessageProtocol.PROTOCOL_DATA1] = 0x00;
+            Message[MessageProtocol.PROTOCOL_DATA2] = 0x00;
+            Message[MessageProtocol.PROTOCOL_CHECKSUM] = 0;
+            for (int i = 0; i < MessageProtocol.PROTOCOL_CHECKSUM; i++)
+                Message[MessageProtocol.PROTOCOL_CHECKSUM] ^= Message[i];       // XOR
+            Message[MessageProtocol.PROTOCOL_TAIL] = MessageProtocol.TAIL;
+
+            return Message;
+        }
+
+        // 제어장치에게 시작 명령
+        public static byte[] Meesage_DeviceStart(bool start)
+        {
+            byte[] Message = new byte[7];
+
+            Message[MessageProtocol.PROTOCOL_HEADER] = MessageProtocol.HEADER;
+            Message[MessageProtocol.PROTOCOL_CMD] = MessageProtocol.CMD_ORDER;
+            if(start)
+                Message[MessageProtocol.PROTOCOL_REG] = MessageProtocol.CMD_ORDER_START;
+            else
+                Message[MessageProtocol.PROTOCOL_REG] = MessageProtocol.CMD_ORDER_STOP;
+            Message[MessageProtocol.PROTOCOL_DATA1] = 0x00;
+            Message[MessageProtocol.PROTOCOL_DATA2] = 0x00;
+            Message[MessageProtocol.PROTOCOL_CHECKSUM] = 0;
+            for (int i = 0; i < MessageProtocol.PROTOCOL_CHECKSUM; i++)
+                Message[MessageProtocol.PROTOCOL_CHECKSUM] ^= Message[i];       // XOR
+            Message[MessageProtocol.PROTOCOL_TAIL] = MessageProtocol.TAIL;
+
+            return Message;
+        }
+
+        // 제어장치에게 알림
+        public static byte[] Meesage_Infom(byte inform)
+        {
+            byte[] Message = new byte[7];
+
+            Message[MessageProtocol.PROTOCOL_HEADER] = MessageProtocol.HEADER;
+            Message[MessageProtocol.PROTOCOL_CMD] = MessageProtocol.CMD_ORDER;
+            Message[MessageProtocol.PROTOCOL_REG] = inform;
+            Message[MessageProtocol.PROTOCOL_DATA1] = 0x00;
+            Message[MessageProtocol.PROTOCOL_DATA2] = 0x00;
+            Message[MessageProtocol.PROTOCOL_CHECKSUM] = 0;
+            for (int i = 0; i < MessageProtocol.PROTOCOL_CHECKSUM; i++)
+                Message[MessageProtocol.PROTOCOL_CHECKSUM] ^= Message[i];       // XOR
+            Message[MessageProtocol.PROTOCOL_TAIL] = MessageProtocol.TAIL;
+
+            return Message;
+        }
+
+        // 제어장치에게 특정 값 쓰기 요청
+        public static byte[] Meesage_Write(byte Reg, short data)
+        {
+            byte[] Message = new byte[7];
+
+            Message[MessageProtocol.PROTOCOL_HEADER] = MessageProtocol.HEADER;
+            Message[MessageProtocol.PROTOCOL_CMD] = MessageProtocol.CMD_ORDER;
+            Message[MessageProtocol.PROTOCOL_REG] = Reg;
+            Message[MessageProtocol.PROTOCOL_DATA1] = (byte)(data>>8);
+            Message[MessageProtocol.PROTOCOL_DATA2] = (byte)data;
+            Message[MessageProtocol.PROTOCOL_CHECKSUM] = 0;
+            for (int i = 0; i < MessageProtocol.PROTOCOL_CHECKSUM; i++)
+                Message[MessageProtocol.PROTOCOL_CHECKSUM] ^= Message[i];       // XOR
+            Message[MessageProtocol.PROTOCOL_TAIL] = MessageProtocol.TAIL;
+
+            return Message;
+        }
+
+        // 제어장치에게 특정 값 읽기 요청
+        public static byte[] Meesage_Read(byte Reg)
+        {
+            byte[] Message = new byte[7];
+
+            Message[MessageProtocol.PROTOCOL_HEADER] = MessageProtocol.HEADER;
+            Message[MessageProtocol.PROTOCOL_CMD] = MessageProtocol.CMD_ORDER;
+            Message[MessageProtocol.PROTOCOL_REG] = Reg;
+            Message[MessageProtocol.PROTOCOL_DATA1] = 0x00;
+            Message[MessageProtocol.PROTOCOL_DATA2] = 0x00;
+            Message[MessageProtocol.PROTOCOL_CHECKSUM] = 0;
+            for (int i = 0; i < MessageProtocol.PROTOCOL_CHECKSUM; i++)
+                Message[MessageProtocol.PROTOCOL_CHECKSUM] ^= Message[i];       // XOR
+            Message[MessageProtocol.PROTOCOL_TAIL] = MessageProtocol.TAIL;
+
+            return Message;
+        }
+
+
+    }
+}
