16bd83bcadf72d181539787a0af7db5ab1f6c420
yeonSG <dustjd43@gmail.com>
2020-12-10 02:45:16 +0900

    전역 예외처리

--- TubeFeederProject/TubeFeeder/Program.cs
+++ TubeFeederProject/TubeFeeder/Program.cs
@@ -13,7 +13,25 @@ namespace TubeFeeder
         [MTAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             Application.Run(new Form1());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            SmartX.SmartFile m_smartFile = m_smartFile = new SmartX.SmartFile();
+
+            m_smartFile.FilePathName = "\\Flash Disk\\ErrorLog.txt";
+            if (m_smartFile.Open() == true)
+            {
+                m_smartFile.WriteString(e.ToString());
+                m_smartFile.Close();
+            }
+            else
+            {
+                ;
+            }            
+            // 처리되지 않는 모든 예외처리 코드 작성
         }
     }
 }
